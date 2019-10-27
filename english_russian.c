4e46855619311ffa831b67c00c730903306f84a8
HollyLee2000 <1224563290@qq.com>
2019-10-27 13:06:14 +0800

    week 8 homework

--- english_russian.c
+++ english_russian.c
@@ -6,3 +6,45 @@
 //     è¾“å…¥ä¸¤æ ·éƒ½ä¸ä¼šæœ‰8äºº
 //     è¾“å‡ºä¸¤æ ·éƒ½ä¼šçš„æœ‰   äºº 
 //     è¾“å‡ºï¼šè¾“å…¥æ•°æ®é”™è¯¯ï¼Œæ— æ³•è®¡ç®—
+
+#include<stdio.h>
+int main()
+{
+	int english, russian, traveler, bothcannot, bothcan;
+	printf("ÊäÈëÂÃĞĞÉçµÄÈËÊı\n");
+	while((scanf("%d",&traveler))!=1||traveler < 0)
+	{
+		while(getchar()!='\n')
+			continue;
+		printf("ÊäÈë´íÎó£¬ÇëÖØĞÂÊäÈë£¡\n");
+	}
+	printf("ÊäÈë»áÓ¢ÓïµÄÈËÊı\n");
+	while((scanf("%d",&english))!=1||english < 0)
+	{
+		while(getchar()!='\n')
+			continue;
+		printf("ÊäÈë´íÎó£¬ÇëÖØĞÂÊäÈë£¡\n");
+	}
+	printf("ÊäÈë»á¶íÓïµÄÈËÊı\n");
+	while((scanf("%d",&russian))!=1||russian < 0)
+	{
+		while(getchar()!='\n')
+			continue;
+		printf("ÊäÈë´íÎó£¬ÇëÖØĞÂÊäÈë£¡\n");
+	}
+	printf("ÊäÈëÁ½ÖÖÓïÑÔ¶¼²»»áµÄÈËÊı\n");
+	while((scanf("%d",&bothcannot))!=1||bothcannot < 0||bothcannot > traveler)
+	{
+		while(getchar()!='\n')
+			continue;
+		printf("ÊäÈë´íÎó£¬ÇëÖØĞÂÊäÈë£¡\n");
+	}
+	bothcan = (english + russian) - (traveler - bothcannot);
+	if(bothcan < 0)
+	{
+		printf("ÊäÈëÊı¾İ´íÎóÎŞ·¨¼ÆËã\n");
+	}
+	else
+		printf("Á½ÖÖÓïÑÔ¶¼»áµÄÈËÓĞ%d¸ö\n",bothcan); 
+	return 0;
+}
--- sort_by_exchange.cpp
+++ sort_by_exchange.cpp
@@ -1,19 +1,24 @@
 //交换排序
 #include <stdio.h>
-int main() {
-  int x[3] = {5, 2, 1}, i;
-  while(1) {
-    scanf("%d %d %d", &x[0], &x[1], &x[2]);
-    for(i=0;i<2;i++) {
-      if( x[i] > x[i+1]) {
-  	    int temp = x[i];
-  	    x[i] = x[i+1];
-  	    x[i+1] = temp;
-      }
-      i = i + 1;
-      i = i - 1;
-    }
-    printf(" %d, %d, %d", x[0], x[1], x[2]);
-  }
-  return 0;
+int main() 
+{
+	int x[3], i, j, temp;
+  	while(1)
+   	{
+		scanf("%d %d %d", &x[0], &x[1], &x[2]);
+    	for(i=0;i<3;i++) 
+		{
+			for(j = 0; j < i; j++)
+			{
+      			if( x[j] > x[i]) 
+				{
+  	    			temp = x[i];
+  	    			x[i] = x[j];
+  	    			x[j] = temp;
+  	    		}
+      		}
+    	}
+    	printf("%d %d %d\n", x[0], x[1], x[2]);
+  	}
+  	return 0;
 }
